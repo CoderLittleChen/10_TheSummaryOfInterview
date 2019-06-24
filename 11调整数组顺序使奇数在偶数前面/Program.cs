13fb9c88c9c5f41b1ec1d1c3794c0e9aced8fe0f
cm1415926 <603568511@qq.com>
2019-06-24 11:02:44 +0800

    2019.06.24

---
+++ 11调整数组顺序使奇数在偶数前面/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11调整数组顺序使奇数在偶数前面
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 4, 6, 7, 9, 13, 16, 19, 21, 20, 22, 37 };
+            Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i] + "  ");
+            }
+            Console.ReadKey();
+        }
+
+        public static void Sort(int[] nums)
+        {
+            int i = 0;
+            int j = nums.Length - 1;
+            int temp;
+            while (i < j)
+            {
+                while (nums[j] % 2 == 0)
+                {
+                    j--;
+                }
+                while (nums[i] % 2 != 0)
+                {
+                    i++;
+                }
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+
+        }
+
+    }
+}
---
+++ 12找出数组中重复次数为奇数的元素/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12找出数组中重复次数为奇数的元素
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 1, 1, 2, 2, 3, 3, 3, 5, 5, 6, 4, 4, 6 };
+        }
+
+        public static int SearchItem(int[] nums)
+        {
+            //可以先快速排序   然后改进一下二分查找
+            return 0;
+        }
+
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <returns></returns>
+        public static int QuickSort(int[] nums, int low, int high)
+        {
+            int i = low, j = high, temp;
+            temp = nums[i];
+            while (i < j)
+            {
+                while (i < j && nums[j] >= temp)
+                {
+                    j--;
+                }
+                nums[i] = nums[j];
+                while (i < j && nums[i] <= temp)
+                {
+                    i++;
+                }
+                nums[j] = nums[i];
+            }
+            nums[i] = temp;
+            return i;
+        }
+
+    }
+}
