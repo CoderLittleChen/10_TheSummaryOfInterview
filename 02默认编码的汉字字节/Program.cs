305afcbb76edfa8516ac98217923ab5c883015d4
cm1415926 <603568511@qq.com>
2019-06-27 16:46:54 +0800

    2019.06.27

--- 02默认编码的汉字字节/Program.cs
+++ 02默认编码的汉字字节/Program.cs
@@ -11,19 +11,21 @@ namespace _02默认编码的汉字字节
         static void Main(string[] args)
         {
             string str = "汉子对什么啊";
-            //这里的默认编码就是当前电脑的默认编码，为GB2312
+            //这里的默认编码就是当前电脑的默认编码，为GB2312   汉字也是2字节
 
-            //ASCII编码中，英文和汉子所占的编码数是一样的 1字节
+            //ASCII编码中，英文和汉字所占的编码数是一样的 1字节
             //Unicode编码中，汉子和英文都是2字节
-            //int i = Encoding.Default.GetBytes(str).Length;
-            //int j = str.Length;
-            //Console.WriteLine("i=" + i + "  j=" + j);
-            //Console.WriteLine(Encoding.Default.EncodingName);
+            int i = Encoding.Default.GetBytes(str).Length;
+            int j = str.Length;
+            Console.WriteLine("i=" + i + "  j=" + j);
+            Console.WriteLine(Encoding.Default.EncodingName);
+            Console.ReadKey();
+
+            //int n = Convert.ToInt32("112300", 10);
+            //Console.WriteLine(n);
             //Console.ReadKey();
 
-            int n = Convert.ToInt32("112300", 10);
-            Console.WriteLine(n);
-            Console.ReadKey();
+            
 
         }
     }
---
+++ 12decimal和double/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12decimal和double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //重写看一下  数据转换  decimal  double类型的区别     
+            //为什么  5226.925  和526.925的结果不一样？
+            //decimal d = Math.Round((decimal)526.925,2, MidpointRounding.AwayFromZero);
+            //Console.WriteLine(d);
+            //Console.ReadKey();
+        }
+    }
+}
